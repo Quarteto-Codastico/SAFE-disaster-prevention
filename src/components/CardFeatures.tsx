7db3ef7ba39a68a2587643b255aa264d45ddb2df
Wendel_dev <tybnkdbj35@gmail.com>
2024-08-03 19:38:02 -0300

    feat: add features component

---
+++ src/components/CardFeatures.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+
+interface CardFeaturesProps {
+    icon: ReactNode;
+	title: string;
+    desc: string;
+}
+
+export const CardFeatures: React.FC<CardFeaturesProps> = ({title,icon,desc}) => {
+  return (
+    <div className="flex flex-col h-[281px] gap-6 w-[240px]">
+        <div className="flex w-[79px] h-[79px] justify-center items-center text-white bg-[#75FFCA] rounded-[10px]
+                        text-5xl">{icon}</div>
+        <h1 className="text-[#5B5B5B] text-2xl font-semibold">{title}</h1>
+        <p className="text-[#5B5B5B] text-base font-light text-justify">{desc}</p>
+    </div>
+  )
+}
---
+++ src/components/Features.tsx
@@ -0,0 +1,31 @@
+import { CardFeatures } from "./CardFeatures";
+import { FaMapLocationDot } from "react-icons/fa6";
+import { RiAlertFill } from "react-icons/ri";
+import { BiSolidDonateHeart } from "react-icons/bi";
+
+export default function Features() {
+	return (
+		<div className="flex flex-col h-[562px] max-w-[1600px] w-[100%] px-[79px] justify-center mx-auto">
+            <h1 className="text-[#5B5B5B] font-semibold text-2xl pb-14 pl-9">Conheça as Funcionalidades</h1>
+			<div className="flex gap-32 items-center justify-between self-center">
+				<CardFeatures
+					icon={<FaMapLocationDot />}
+					title="Localize Ocorrências"
+					desc="Localize em tempo real possíveis acontecimentos perto da sua localização e evite supresas arriscadas"
+				/>
+				<div className="h-[150px] w-[1px] border-[1px] border-l-[#5B5B5B]"></div>
+				<CardFeatures
+					icon={<RiAlertFill />}
+					title="Crie Alertas"
+					desc="Avise outras pessoas perto de você sobre algo de extremo risco que pode impacta-las, evitando acidentes indesejados."
+				/>
+                <div className="h-[150px] w-[1px] border-[1px] border-l-[#5B5B5B]"></div>
+				<CardFeatures
+					icon={<BiSolidDonateHeart />}
+					title="Faça Doações"
+					desc="Ajude de forma solidária pessoas vítimas de catástrofes e causas que necessitam de apoio financeiro."
+				/>
+			</div>
+		</div>
+	);
+}
