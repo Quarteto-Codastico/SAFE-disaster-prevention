769b487e99e11324b40cdf3418de3e073e2bff8c
Jhonatas J. Mendonça <118845977+Jhonatas56@users.noreply.github.com>
2024-08-04 16:56:24 -0300

    chore: add localhost domain in next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   env: {
     API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
