a1542511c829c601be21bde4c8bc397ae4ab5e4a
Wendel_dev <tybnkdbj35@gmail.com>
2024-08-04 10:45:04 -0300

    feat: add user alerts page

---
+++ src/app/dashboard/alerts/page.tsx
@@ -0,0 +1,26 @@
+import { FaPlus } from "react-icons/fa";
+import dynamic from "next/dynamic";
+
+const Map = dynamic(
+	() => import("@/components/user-dashboard-components/Map"),
+	{ ssr: false }
+);
+
+export default function alerts() {
+	return (
+		<div className="flex flex-col justify-center items-center w-100%  max-w-[1600px] mt-[110px]">
+			<div className="flex w-[84%] justify-between mr-0 mb-4">
+				<h1 className="text-[#5B5B5B] text-2xl font-bold">
+					Mapa de ocorrências
+				</h1>
+				<button className="flex justify-center items-center font-semibold gap-4 py-2 px-6  bg-clientGradient rounded-[16px] text-[#5B5B5B]">
+					REGISTRAR OCORRÊNCIAS
+					<FaPlus className="text-2xl text-[#5B5B5B]" />
+				</button>
+			</div>
+			<div className="w-[1250px] h-[480px]">
+				<Map />
+			</div>
+		</div>
+	);
+}
