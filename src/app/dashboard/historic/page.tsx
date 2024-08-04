13bb81726b1ac21ae30b600651e3459b315fae67
Wendel_dev <tybnkdbj35@gmail.com>
2024-08-04 14:46:53 -0300

    fix: change in table styling

--- src/app/dashboard/historic/page.tsx
+++ src/app/dashboard/historic/page.tsx
@@ -1,141 +1,165 @@
+"use client";
 import {
 	Table,
 	Thead,
 	Tbody,
-	Tfoot,
 	Tr,
 	Th,
 	Td,
-	TableCaption,
 	TableContainer,
+	useBreakpointValue,
 } from "@chakra-ui/react";
 
 export default function Historic() {
+	// Para garantir que o TableContainer se ajuste ao tamanho da tela
+	const tableContainerProps = useBreakpointValue({
+		base: {
+			borderWidth: "1px",
+			borderRadius: "md",
+			boxShadow: "md",
+			overflowX: "auto",
+		},
+		lg: {
+			borderWidth: "1px",
+			borderRadius: "md",
+			boxShadow: "md",
+			overflowX: "auto",
+		},
+	});
+
 	return (
 		<div className="flex flex-col w-[90%] max-w-[1600px] h-[535px] mx-auto mt-[110px] mb-[170px]">
-			<h1 className="text-[#5B5B5B] text-2xl font-bold self-center mb-8">Histórico de ocorrências</h1>
-			<TableContainer className="border border-[#75FFCA] rounded-lg shadow-lg overflow-x-auto ">
-				<Table variant="striped" size="sm" className="w-full border-collapse">
-					<Thead className="bg-[#75FFCA] border-b border-[#75FFCA]">
+			<h1 className="text-[#5B5B5B] text-2xl font-bold self-center mb-8">
+				Histórico de ocorrências
+			</h1>
+			<TableContainer {...tableContainerProps}>
+				<Table variant="striped" colorScheme="#fff">
+					<Thead bg="#75FFCA" color="white">
 						<Tr>
-							<Th className="text-left px-4 py-2 border-r border-[#75FFCA]">
+							<Th textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								Data
 							</Th>
-							<Th className="text-left px-4 py-2 border-r border-[#75FFCA]">
+							<Th textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								Hora
 							</Th>
-							<Th className="text-left px-4 py-2 border-r border-[#75FFCA]">
+							<Th textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								End.
 							</Th>
-							<Th className="text-left px-4 py-2">Tipo</Th>
+							<Th textAlign="center" borderRight="1px" borderColor="#75FFCA">
+								Tipo
+							</Th>
 						</Tr>
 					</Thead>
 					<Tbody>
-						<Tr className="hover:bg-gray-100 border-b border-[#75FFCA]">
-							<Td className="px-4 py-2 border-r border-[#75FFCA]">inches</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]">
+						<Tr _hover={{ bg: "#fff" }}>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
+								inches
+							</Td>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								millimetres (mm)
 							</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]" isNumeric>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								25.4
 							</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]" isNumeric>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								null
 							</Td>
 						</Tr>
-						<Tr className="hover:bg-gray-100 border-b border-[#75FFCA]">
-							<Td className="px-4 py-2 border-r border-[#75FFCA]">inches</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]">
+						<Tr _hover={{ bg: "#fff" }}>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
+								inches
+							</Td>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								millimetres (mm)
 							</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]" isNumeric>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								25.4
 							</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]" isNumeric>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								null
 							</Td>
 						</Tr>
-						<Tr className="hover:bg-gray-100 border-b border-[#75FFCA]">
-							<Td className="px-4 py-2 border-r border-[#75FFCA]">inches</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]">
+						<Tr _hover={{ bg: "#fff" }}>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
+								inches
+							</Td>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								millimetres (mm)
 							</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]" isNumeric>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								25.4
 							</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]" isNumeric>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								null
 							</Td>
 						</Tr>
-						<Tr className="hover:bg-gray-100 border-b border-[#75FFCA]">
-							<Td className="px-4 py-2 border-r border-[#75FFCA]">inches</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]">
+						<Tr _hover={{ bg: "#fff" }}>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
+								inches
+							</Td>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								millimetres (mm)
 							</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]" isNumeric>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								25.4
 							</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]" isNumeric>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								null
 							</Td>
 						</Tr>
-						<Tr className="hover:bg-gray-100 border-b border-[#75FFCA]">
-							<Td className="px-4 py-2 border-r border-[#75FFCA]">inches</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]">
+						<Tr _hover={{ bg: "#fff" }}>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
+								inches
+							</Td>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								millimetres (mm)
 							</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]" isNumeric>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								25.4
 							</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]" isNumeric>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								null
 							</Td>
 						</Tr>
-						<Tr className="hover:bg-gray-100 border-b border-[#75FFCA]">
-							<Td className="px-4 py-2 border-r border-[#75FFCA]">inches</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]">
+						<Tr _hover={{ bg: "#fff" }}>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
+								inches
+							</Td>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								millimetres (mm)
 							</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]" isNumeric>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								25.4
 							</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]" isNumeric>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								null
 							</Td>
 						</Tr>
-						<Tr className="hover:bg-gray-100 border-b border-[#75FFCA]">
-							<Td className="px-4 py-2 border-r border-[#75FFCA]">inches</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]">
+						<Tr _hover={{ bg: "#fff" }}>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
+								inches
+							</Td>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								millimetres (mm)
 							</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]" isNumeric>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								25.4
 							</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]" isNumeric>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								null
 							</Td>
 						</Tr>
-						<Tr className="hover:bg-gray-100 border-b border-[#75FFCA]">
-							<Td className="px-4 py-2 border-r border-[#75FFCA]">inches</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]">
+						<Tr _hover={{ bg: "#fff" }}>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
+								inches
+							</Td>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								millimetres (mm)
 							</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]" isNumeric>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								25.4
 							</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]" isNumeric>
-								null
-							</Td>
-						</Tr>
-						<Tr className="hover:bg-gray-100 border-b border-[#75FFCA]">
-							<Td className="px-4 py-2 border-r border-[#75FFCA]">inches</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]">
-								millimetres (mm)
-							</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]" isNumeric>
-								25.4
-							</Td>
-							<Td className="px-4 py-2 border-r border-[#75FFCA]" isNumeric>
+							<Td textAlign="center" borderRight="1px" borderColor="#75FFCA">
 								null
 							</Td>
 						</Tr>
