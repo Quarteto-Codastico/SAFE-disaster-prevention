614790285078b96bbf41ef944daeee0ed40c4a6a
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2024-08-04 18:09:51 -0300

    fix: add bg auth

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,7 @@ const config: Config = {
       backgroundImage: {
         clientGradient:
           "var(--gradientSafe, linear-gradient(285deg, #FFF -31.98%, #75FFCA 51.24%, #3FFFB6 102.03%))",
+        bg_auth: "url('../../public/fundo_form.png')",
         adminGradient:
           "var(--gradientAdm, linear-gradient(285deg, #000 -31.98%, #3E8D6F 51.24%, #1D654A 102.03%))",
       },
