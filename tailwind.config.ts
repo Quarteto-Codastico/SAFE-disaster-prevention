95dd43191ac951debaf5b464ad1cb99db06a65be
Wendel_dev <tybnkdbj35@gmail.com>
2024-08-04 10:48:30 -0300

    feat: add component libs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        clientGradient: "var(--gradientSafe, linear-gradient(285deg, #FFF -31.98%, #75FFCA 51.24%, #3FFFB6 102.03%))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/page.tsx",
+		"./src/app/layout.tsx",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				clientGradient:
+					"var(--gradientSafe, linear-gradient(285deg, #FFF -31.98%, #75FFCA 51.24%, #3FFFB6 102.03%))",
+				adminGradient:
+					"var(--gradientAdm, linear-gradient(285deg, #000 -31.98%, #3E8D6F 51.24%, #1D654A 102.03%))",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
