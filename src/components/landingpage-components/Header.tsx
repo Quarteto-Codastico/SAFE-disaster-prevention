f12a3a62c9c2c73cfe0883cb881d6ff9b82c66d5
Jhonatas J. Mendonça <118845977+Jhonatas56@users.noreply.github.com>
2024-08-04 15:16:33 -0300

    feat: add redirect functions in landing page header buttons

--- src/components/landingpage-components/Header.tsx
+++ src/components/landingpage-components/Header.tsx
@@ -1,34 +1,66 @@
+"use client";
+
 import Image from "next/image";
 import logoSafe from "../../../public/logo_safe.png";
 import { IoIosArrowDown } from "react-icons/io";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
-	return (
-		<div className="flex fixed w-screen justify-center items-center h-[80px] bg-white px-20 z-10">
-			<div className="flex max-w-[1440px] w-[100%] h-[80px] justify-between items-center">
-				<div className="flex flex-2 h-[80px] justify-start items-center">
-					<Image src={logoSafe} alt="logo-safe" className="h-[49px] w-[119px]" />
-				</div>
-				<div className="flex flex-1 gap-8 justify-center items-center">
-					<a href="#hero" className="flex gap-3 justify-center items-center text-sm">
-						SOBRE <IoIosArrowDown />
-					</a>
-					<a href="#features" className="flex gap-3 justify-center items-center text-sm">
-						FUNCIONALIDADES <IoIosArrowDown />
-					</a>
-					<a href="#donate" className="flex gap-3 justify-center items-center text-sm">
-						DOAÇÔES <IoIosArrowDown />
-					</a>
-				</div>
-				<div className="flex flex-2 justify-end  items-center gap-6">
-					<button className="py-3 px-8 border-[2px] border-[#000] rounded-[16px]">
-						Entrar
-					</button>
-					<button className="py-3 px-12  bg-[#3FFFB6] rounded-[16px] text-white">
-						CRIAR CONTA
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+  const router = useRouter();
+
+  function handleRedirectToLogin() {
+    router.push("/sign-in");
+  }
+
+  function handleRedirectToSignUp() {
+    router.push("/sign-up");
+  }
+
+  return (
+    <div className="flex fixed w-screen justify-center items-center h-[80px] bg-white px-20 z-10">
+      <div className="flex max-w-[1440px] w-[100%] h-[80px] justify-between items-center">
+        <div className="flex flex-2 h-[80px] justify-start items-center">
+          <Image
+            src={logoSafe}
+            alt="logo-safe"
+            className="h-[49px] w-[119px]"
+          />
+        </div>
+        <div className="flex flex-1 gap-8 justify-center items-center">
+          <a
+            href="#hero"
+            className="flex gap-3 justify-center items-center text-sm"
+          >
+            SOBRE <IoIosArrowDown />
+          </a>
+          <a
+            href="#features"
+            className="flex gap-3 justify-center items-center text-sm"
+          >
+            FUNCIONALIDADES <IoIosArrowDown />
+          </a>
+          <a
+            href="#donate"
+            className="flex gap-3 justify-center items-center text-sm"
+          >
+            DOAÇÔES <IoIosArrowDown />
+          </a>
+        </div>
+        <div className="flex flex-2 justify-end  items-center gap-6">
+          <button
+            className="py-3 px-8 border-[2px] border-[#000] rounded-[16px]"
+            onClick={handleRedirectToLogin}
+          >
+            Entrar
+          </button>
+          <button
+            className="py-3 px-12  bg-[#3FFFB6] rounded-[16px] text-white"
+            onClick={handleRedirectToSignUp}
+          >
+            CRIAR CONTA
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
