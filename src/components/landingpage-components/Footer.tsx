cf33baccba38e4d7d1b5667c7960b3c30af0f63f
Wendel_dev <tybnkdbj35@gmail.com>
2024-08-04 17:54:56 -0300

    feat: add responsiveness

--- src/components/landingpage-components/Footer.tsx
+++ src/components/landingpage-components/Footer.tsx
@@ -3,15 +3,17 @@ import footer_bg from "../../../public/footer_bg.svg";
 
 export default function Footer() {
 	return (
-		<div className="flex relative w-[100%] max-w-[1600px] mx-auto">
-			<div className="flex justify-between absolute top-[75%] -translate-y-1/2 left-[50%] -translate-x-1/2 w-[70%] font-light">
-				<p className="flex flex-1 justify-center min-w-[400px]">
+		<div className=" relative w-[100%] max-w-[1600px] mx-auto ">
+			<div className="flex justify-between absolute top-[75%] -translate-y-1/2 left-[50%] -translate-x-1/2 w-[100%] font-light
+							 tablet:text-[10px] phone:text-[10px]">
+				<p className="flex flex-1 justify-center min-w-[400px] phone:hidden tablet:min-w-[200px]">
 					SAFE prevenção de catástrofes
 				</p>
-				<p className="flex flex-1 justify-center">|</p>
-				<p className="flex flex-1 justify-center"> © 2024</p>
-				<p className="flex flex-1 justify-center ">|</p>
-				<p className="flex flex-1 justify-center min-w-[400px]">
+				<p className="flex flex-1 justify-center phone:min-w-[200px] phone:hidden tablet:hidden">|</p>
+				<p className="flex flex-1 justify-center phone:hidden"> © 2024</p>
+				<p className="flex flex-1 justify-center phone:hidden tablet:hidden">|</p>
+				<p className="flex flex-1 justify-center min-w-[400px] phone:justify-center phone:min-w-[200px] 
+							phone:pr-4 tablet:min-w-[200px]">
 					Desenvolvido por Quarteto Codástico
 				</p>
 			</div>
--- src/components/landingpage-components/Hero.tsx
+++ src/components/landingpage-components/Hero.tsx
@@ -1,20 +1,57 @@
+"use client";
 import Image from "next/image";
 import backgorundHero from "../../../public/fundo_hero.png";
+import { useRouter } from "next/navigation";
+
 
 export default function Hero() {
+	const router = useRouter();
+
+	function handleRedirectToLogin() {
+		router.push("/sign-in");
+	}
+
+	function handleRedirectToSignUp() {
+		router.push("/sign-up");
+	}
+
 	return (
-		<div id='hero' className="flex relative max-w-[1600px] w-[100%] h-[700px] bg-clientGradient self-center mx-auto pt-[80px]">
-			<div className="absolute flex gap-8 flex-col pl-16 w-[870px] top-[50%] -translate-y-1/2 pt-[80px]">
-				<h1 className="text-white text-5xl font-bold ">
+		<div
+			id="hero"
+			className="relative max-w-[1600px] w-full h-[700px] bg-clientGradient mx-auto pt-[80px] overflow-hidden"
+		>
+			<div
+				className="absolute flex gap-8 flex-col pl-16 w-[60%] top-[50%] -translate-y-1/2 pt-[80px] phone:w-[95%] phone:top-[20rem] phone:pl-4 phone:text-center
+							phone:pt-0"
+			>
+				<h1 className="text-white text-5xl font-bold lg:text-4xl tablet:text-3xl phone:text-4xl">
 					Uma plataforma feita pra salvar vidas! Ajude-nos a tornar o mundo um
 					lugar seguro.
 				</h1>
-				<p className="text-[#5B5B5B] text-2xl w-[70%] font-light">
+				<p className="text-[#5B5B5B] text-2xl w-[70%] font-light phone:font-extralight phone:text-xl phone:mx-auto">
 					Juntos, construindo resiliência e segurança para enfrentar os desafios
 					das catástrofes naturais e humanas.
 				</p>
 			</div>
-			<Image src={backgorundHero} alt="fundo-hero" className="absolute self-end right-0 bottom-0"/>
+			<Image
+				src={backgorundHero}
+				alt="fundo-hero"
+				className="absolute self-end right-0 bottom-0 tablet:left-[30rem] lg:left-[50rem] phone:hidden"
+			/>
+			<div className="hidden w-[100%] absolute bottom-14 phone:flex flex-2 justify-center items-center gap-6">
+				<button
+					className="py-3 px-8 border-[2px] border-[#fff] text-[#fff] rounded-[16px]"
+					onClick={handleRedirectToLogin}
+				>
+					Entrar
+				</button>
+				<button
+					className="py-3 px-12  bg-[#39e7a4] rounded-[16px] text-white"
+					onClick={handleRedirectToSignUp}
+				>
+					CRIAR CONTA
+				</button>
+			</div>
 		</div>
 	);
 }
