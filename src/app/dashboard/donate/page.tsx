825e050fb8af78e82c0a9fcee14c1bde70ed3855
Wendel_dev <tybnkdbj35@gmail.com>
2024-08-04 14:46:15 -0300

    feat: add donate modal

--- src/app/dashboard/donate/page.tsx
+++ src/app/dashboard/donate/page.tsx
@@ -1,7 +1,11 @@
+"use client";
 import { CardDonate } from "@/components/user-dashboard-components/CardDonate";
 import dog from "../../../../public/cachorro-filhote.jpg";
+import qrCodeDogs from "../../../../public/qr_code.png";
 
-export default function donate() {
+
+export default function Donate() {
+	
 	return (
 		<div className="flex flex-col justify-center items-center w-100%  max-w-[1600px] mt-[110px]">
 			<h1 className="text-[#5B5B5B] text-2xl font-bold self-center mb-8">
@@ -13,31 +17,49 @@ export default function donate() {
 					title="Ajude os doguinhos"
 					desc="faça uma doação para ajudar esses peludinhos"
 					icon={dog}
+					qrCode={qrCodeDogs}
+					recebedor="xxxxxxxxxxxxxxxxxxx"
+					loc="https://maps.app.goo.gl/Qqqjob9FaYQPdxN16"
 				/>
 				<CardDonate
 					title="Ajude os doguinhos"
 					desc="faça uma doação para ajudar esses peludinhos"
 					icon={dog}
+					qrCode={qrCodeDogs}
+					recebedor="xxxxxxxxxxxxxxxxxxx"
+					loc="https://maps.app.goo.gl/Qqqjob9FaYQPdxN16"
 				/>
 				<CardDonate
 					title="Ajude os doguinhos"
 					desc="faça uma doação para ajudar esses peludinhos"
 					icon={dog}
+					qrCode={qrCodeDogs}
+					recebedor="xxxxxxxxxxxxxxxxxxx"
+					loc="https://maps.app.goo.gl/Qqqjob9FaYQPdxN16"
 				/>
 				<CardDonate
 					title="Ajude os doguinhos"
 					desc="faça uma doação para ajudar esses peludinhos"
 					icon={dog}
+					qrCode={qrCodeDogs}
+					recebedor="xxxxxxxxxxxxxxxxxxx"
+					loc="https://maps.app.goo.gl/Qqqjob9FaYQPdxN16"
 				/>
 				<CardDonate
 					title="Ajude os doguinhos"
 					desc="faça uma doação para ajudar esses peludinhos"
 					icon={dog}
+					qrCode={qrCodeDogs}
+					recebedor="aaaaaaaaaaaaaaaaaaaaaaa"
+					loc="https://maps.app.goo.gl/Qqqjob9FaYQPdxN16"
 				/>
 				<CardDonate
 					title="Ajude os doguinhos"
 					desc="faça uma doação para ajudar esses peludinhos"
 					icon={dog}
+					qrCode={qrCodeDogs}
+					recebedor="xxxxxxxxxxxxxxxxxxx"
+					loc="https://maps.app.goo.gl/Qqqjob9FaYQPdxN16"
 				/>
 			</div>
 		</div>
--- src/components/user-dashboard-components/CardDonate.tsx
+++ src/components/user-dashboard-components/CardDonate.tsx
@@ -1,32 +1,105 @@
-import Image, { ImageProps, StaticImageData } from "next/image";
-import { ReactNode } from "react";
+"use client";
+import Image, { StaticImageData } from "next/image";
+import {
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalFooter,
+	ModalBody,
+	ModalCloseButton,
+	Button,
+	useDisclosure,
+} from "@chakra-ui/react";
+
+import { FaMapMarkedAlt } from "react-icons/fa";
+import Link from "next/link";
 
 interface CardDonateProps {
 	icon: StaticImageData;
+	qrCode: StaticImageData;
 	desc: string;
 	title: string;
+	recebedor: string;
+	loc: string;
 }
 
 export const CardDonate: React.FC<CardDonateProps> = ({
 	icon,
 	title,
 	desc,
+	qrCode,
+	recebedor,
+	loc,
 }) => {
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
 	return (
-		<div
-			className={`relative flex flex-col bg-clientGradient w-[400px] h-[200px] rounded-bl-[20px] rounded-br-[20px] rounded-tr-[20px] overflow-hidden`}
-		>
-			<div className="my-auto text-[#5b5b5b] self-end w-[250px]">
-				<h1 className=" font-bold text-3xl pl-4">
-					{title}
-				</h1>
-				<p className=" text-sm pl-4 ">{desc}</p>
+		<>
+			<div
+				onClick={onOpen}
+				className="relative flex flex-col bg-clientGradient w-[400px] h-[200px] rounded-bl-[20px] rounded-br-[20px] rounded-tr-[20px] overflow-hidden cursor-pointer"
+			>
+				<div className="my-auto text-[#5b5b5b] self-end w-[250px]">
+					<h1 className="font-bold text-3xl pl-4">{title}</h1>
+					<p className="text-sm pl-4">{desc}</p>
+				</div>
+				<Image
+					src={icon}
+					alt="icon"
+					className="absolute bottom-[3rem] right-[16rem] w-[180px] h-[180px] rounded-full"
+				/>
 			</div>
-			<Image
-				src={icon}
-				alt="icon"
-				className={`absolute bottom-[3rem] right-[16rem] w-[180px] h-[180px] rounded-full`}
-			/>
-		</div>
+
+			<Modal
+				isCentered
+				onClose={onClose}
+				isOpen={isOpen}
+				motionPreset="slideInBottom"
+			>
+				<ModalOverlay />
+				<ModalContent width="394px">
+					<ModalHeader>
+						<h1 className="text-[#5B5B5B] text-2xl font-bold self-center">
+							Realize uma doação para:
+						</h1>
+					</ModalHeader>
+					<ModalCloseButton color="#FF5A5A" />
+					<ModalBody>
+						<h1 className="text-[#5B5B5B] text-xl font-bold self-center text-center">
+							{title}
+						</h1>
+						<p className="text-sm px-16 text-center font-light">{desc}</p>
+						<div className="flex justify-center mx-auto w-[150px] h-[130px] mt-6 relative">
+							<Image
+								src={icon}
+								alt="icon"
+								className="w-[130px] h-[130px] rounded-[100%] mx-auto"
+							/>
+							<Link href={`${loc}`} className="flex justify-between items-center rounded-xl absolute bg-[#3FFFB6] w-[90%] h-[25px] px-2 top-[7.3rem]">
+								
+									<p className="text-sm font-semibold text-[#5B5B5B]">
+										Ver no Mapa
+									</p>
+									<FaMapMarkedAlt className="text-[#5B5B5B]" />
+							</Link>
+						</div>
+						<h1 className="text-[#A8A8A8] text-lg font-bold self-center text-center mt-6">
+							Nome do recebedor
+						</h1>
+						<p className="text-sm text-center font-semibold text-[#5B5B5B] mb-3">
+							{recebedor}
+						</p>
+						<div className="border-[1px] border-[#A8A8A8] rounded-lg mx-auto w-[150px] mb-6">
+							<Image
+								src={qrCode}
+								alt="qr-code"
+								className="h-[150px] w-[150px] p-2 "
+							/>
+						</div>
+					</ModalBody>
+				</ModalContent>
+			</Modal>
+		</>
 	);
 };
